093c8b9fb2bdd098c9d3c1ff83130cebac2052be
Yuan Ruo <yuanruo@data.gov.sg>
2024-02-26 20:45:43 +0800

    refactor: use PlumberError toString() method

--- src/errors/ErrorReporter.ts
+++ src/errors/ErrorReporter.ts
@@ -7,23 +7,9 @@ class ErrorReporter {
   hadRuntimeError = false
   hadResolvingError = false
 
-  report(error: Error): void {
+  report(error: PlumberError): void {
     this.error = error
-
-    let header = ''
-    if (error instanceof SyntaxError && error.line) {
-      header += `[${error.name} (line ${error.line}`
-      if (error.where) header += ` at ${error.where}`
-      header += ')'
-    } else if (error instanceof RuntimeError) {
-      header += `[${error.name} (line ${error.token.line})`
-    } else if (error instanceof CliError) {
-      header += `[${error.name}`
-    } else {
-      header += '[CliError'
-    }
-    header += ']'
-    console.error(header + ' ' + error.message)
+    console.error(`${error.toString()} ${error.message}`)
 
     if (error instanceof RuntimeError) this.hadRuntimeError = true
     else if (error instanceof SyntaxError) this.hadSyntaxError = true
